0dd85377da68a0b1819eee78b15f2a24a5ace4e8
Macarse <macarse@gmail.com>
2008-10-04 16:32:10 -0300

    Added modAction to complete users' CRUD. CHECK TODO's

--- application/models/UsersTable.php
+++ application/models/UsersTable.php
@@ -10,6 +10,42 @@ class Users extends Zend_Db_Table_Abstract
         $where = $this->getAdapter()->quoteInto('CODIGO_USU = ?', $id);
         $this->delete( $where );
     }
+
+    public function getUserByID( $id )
+    {
+        $where = $this->getAdapter()->quoteInto('CODIGO_USU = ?', $id);
+        $row = $this->fetchRow( $where );
+
+        return $row;
+    }
+
+    public function modifyUser( $id, $name, $user, $pass, $lang )
+    {
+        /*TODO: Validaciones.*/
+
+        /*TODO: Ese id existe?*/
+        $where = $this->getAdapter()->quoteInto('CODIGO_USU = ?', $id);
+        $row = $this->fetchRow($where);
+
+        /*Si no se setteó la pass, uso la vieja.*/
+        if ($pass == "")
+        {
+            $pass = $row->PASSWORD_USU;
+        }
+        else
+        {
+            $pass = hash('SHA1', $pass);
+        }
+
+        /*TODO: Pudo actualizar correctamente?*/
+        $this->update(array('NOMBRE_USU'    => $name,
+                            'USUARIO_USU'   => $user,
+                            'PASSWORD_USU'  => $pass,
+                            'IDIOMA_USU'    => $lang ), $where );
+
+        return True;
+    }
+
 }
 
 ?>
--- application/modules/admin/controllers/ModuserController.php
+++ application/modules/admin/controllers/ModuserController.php
@@ -3,6 +3,9 @@ class admin_ModuserController extends Zend_Controller_Action
 {
 
     protected $_form;
+    protected $_showForm = False;
+    /*TODO: Villero*/
+    protected $_id;
 
     public function init()
     {
@@ -27,12 +30,76 @@ class admin_ModuserController extends Zend_Controller_Action
 
     public function listuserAction()
     {
-        $table = new Users();
-        $this->view->users = $table->fetchAll();
+        /*TODO de esta función:
+        * 1) No pude lograr que se muestre o la lista o el form.
+             Ahora va a otra pantalla y queda feo.
+
+          2) Hay que ver qué hacemos con lo de idioma, si es combo, etc
+             El idioma hace cualquiera.
+
+          3) Hay muchas cosas feas para mi gusto. Comenten.
+
+        */
+        /*Si hay parámetros pedir el form*/
+        if ( $this->getRequest()->getParam('id') != null )
+        {
+            $this->_id = $this->getRequest()->getParam('id');
+            if (($this->view->modForm = $this->getModUserForm($this->_id)) != null)
+            {
+                $this->_showForm = True;
+                $this->view->showForm = True;
+            }
+        }
+
+        /*Si no hay que mostrar el form mostrar la lista de users*/
+        if ( !$this->_showForm )
+        {
+            $table = new Users();
+            $this->view->users = $table->fetchAll();
+            $this->view->showForm = False;
+        }
+
+        /*Si viene algo por post, validarlo.*/
+        if ($this->getRequest()->isPost())
+        {
+            if (isset($_POST['ModUserTrack']))
+            {
+                unset($this->view->failedAddUser);
+
+                if ($this->_form->isValid($_POST))
+                {
+                    // process user
+                    $values = $this->_form->getValues();
+                    if ($values['password'] == $values['passwordvrfy'])
+                    {
+                        //Falta agregar idioma.
+                        $userTable = new Users();
+                        $userTable->modifyUser( $this->_id,
+                                            $values['name'],
+                                            $values['username'],
+                                            $values['password'],
+                                            $values['lang'] );
+
+                        /*Se actualizó, volver a mostrar lista de users*/
+                        $this->_showForm = False;
+                        $this->_helper->redirector->gotoUrl('admin/moduser');
+                    }
+                    else
+                    {
+                        $this->view->failedAddUser = 'Las contraseñas no son iguales';
+                    }
+                }
+                else
+                {
+                    $this->view->failedAddUser = 'Algún campo no fue insertado correctamente';
+                }
+            }
+        }
     }
 
     public function removeuserAction()
     {
+        /*TODO: Agregar un "Seguro que desea eliminar?"*/
         if ( $this->getRequest()->getParam('id') === null )
         {
             /*TODO: No sé si está bien hardcodearlo así. Preguntar.*/
@@ -47,4 +114,66 @@ class admin_ModuserController extends Zend_Controller_Action
         $this->_helper->redirector->gotoUrl('admin/moduser');
     }
 
+
+
+    private function getModUserForm( $id )
+    {
+        /*Esto hace una especie de singleton del form a nivel controlador*/
+        if (null !== $this->_form)
+        {
+            return $this->_form;
+        }
+
+        /*Levanto el usuario para completar el form.*/
+        $table = new Users();
+        $user = $table->getUserByID( $id );
+
+        if ( $user === null )
+        {
+            /*TODO: Hardcodeado ok?*/
+            $this->_helper->redirector->gotoUrl('admin/moduser');
+        }
+
+        $this->_form = new Zend_Form();
+        $this->_form->setAction($this->_baseUrl)->setMethod('post');
+
+        $name = $this->_form->createElement('text', 'name', array('label' => 'Nombre'));
+        $name->setValue($user->name() )
+             ->addValidator('alnum')
+             ->addValidator('stringLength', false, array(1, 50))
+             ->setRequired(true)
+             ->addFilter('StringToLower');
+
+        // Create and configure username element:
+        $username = $this->_form->createElement('text', 'username', array('label' => 'Usuario'));
+        $username->setValue($user->user() )
+                 ->addValidator('alnum')
+                 ->addValidator('stringLength', false, array(1, 30))
+                 ->setRequired(true)
+                 ->addFilter('StringToLower');
+
+        // Create and configure password element:
+        $password = $this->_form->createElement('password', 'password', array('label' => 'Clave'));
+        $password->addValidator('StringLength', false, array(1,100));
+
+        $passwordvrfy = $this->_form->createElement('password', 'passwordvrfy', array('label' => 'Repetir Clave'));
+        $passwordvrfy->addValidator('StringLength', false, array(1,100));
+
+        // frutita++
+        $lang = $this->_form->createElement('text', 'lang', array('label' => 'Idioma'));
+        $lang->setValue($user->language() )
+             ->addValidator('StringLength', false, array(1,20))
+             ->setRequired(true);
+
+        // Add elements to form:
+        $this->_form->addElement($name)
+             ->addElement($username)
+             ->addElement($password)
+             ->addElement($passwordvrfy)
+             ->addElement($lang)
+             ->addElement('hidden', 'ModUserTrack', array('values' => 'logPost'))
+             ->addElement('submit', 'Modificar', array('label' => 'Ingresar'));
+
+        return $this->_form;
+    }
 }
