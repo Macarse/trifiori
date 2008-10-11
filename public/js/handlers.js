b0454d50f26a6210ed46b4904c74b11c498c6285
pabloks <pabloks@gmail.com>
2008-10-11 20:27:15 -0300

    agrego lo que no subi antes

--- public/js/handlers.js
+++ public/js/handlers.js
@@ -2,5 +2,30 @@
 
 function onMenuItemClick(p_sType, p_aArgs, p_Data) 
 {
+    /*
+    Handlers del menu
+            Index = this.index 
+            Text = this.cfg.getProperty("text")
+            Value = p_oValue
+    */
+
     window.location = p_Data.url;
+} 
+
+function hide_div(divID)
+{
+	document.getElementById(divID).style.display = "none";
+}
+
+function show_div(divID)
+{
+	document.getElementById(divID).style.display = "";
+}
+
+function show_hide_div(divID)
+{
+	if (document.getElementById(divID).style.display != "none")
+		hide_div(divID);
+	else
+		show_div(divID);
 }
