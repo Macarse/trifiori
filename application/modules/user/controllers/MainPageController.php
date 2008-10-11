0c6dad44aec54fd28f3412a424a53b57db9ee8db
mgv <mvidal@gmail.com>
2008-10-11 03:08:53 -0200

    Borrado de codigo que ya no es necesario (me habia olvidado :))

--- application/modules/user/controllers/MainPageController.php
+++ application/modules/user/controllers/MainPageController.php
@@ -1,19 +1,9 @@
 <?php
 class user_MainPageController extends Trifiori_User_Controller_Action
 {
-    protected $_acl;
-    protected $_username;
-    
     public function init()
     {
         parent::init();
-        $_acl = Zend_Registry::getInstance()->accesslist;
-        $_username = Zend_Registry::getInstance()->name;
-
-        if (! $_acl->isAllowed($_username, 'user'))
-        {
-            $this->_helper->redirector->gotoUrl('default/index');
-        }
     }
     
     public function indexAction()
