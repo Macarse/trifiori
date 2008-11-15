e816c81d5e09bc77d7eef2837fc966d796b37b88
Macarse <macarse@gmail.com>
2008-11-15 17:52:45 -0200

    Validators Translated

--- library/CV/Validate/Carga.php
+++ library/CV/Validate/Carga.php
@@ -1,35 +1,34 @@
 <?php
-require_once 'Zend/Validate/Abstract.php';
 
 class CV_Validate_Carga extends Zend_Validate_Abstract
 {
     const MSG_CARGA = 'msgValidateCarga';
-
-    protected $_messageTemplates = array(
-        self::MSG_CARGA => "Invalid Load",
-    );
+    protected $_messageTemplates = NULL;
 
     public function isValid($value)
     {
+        $errorMsg = Zend_Registry::getInstance()->language->_('Carga Inválida');
+        $this->_messageTemplates = array(self::MSG_CARGA => $errorMsg);
+
         $this->_setValue($value);
 
-		$cargas = new Cargas();
-		try
-		{
-			$codCarga = $cargas->getCargaByNroPaq($value);
-			if ($codCarga != NULL)
-				return true;
-			else
-			{
-		            $this->_error(self::MSG_CARGA);
-		            return false;
-			}
-		}
-		catch (Zend_Exception $e)
+        $cargas = new Cargas();
+        try
+        {
+            $codCarga = $cargas->getCargaByNroPaq($value);
+            if ($codCarga != NULL)
+                return true;
+            else
+            {
+                $this->_error(self::MSG_CARGA);
+                return false;
+            }
+        }
+        catch (Zend_Exception $e)
         {
             throw new Exception($e->getMessage());
             return false;
-		}
+        }
 
     }
 }
--- library/CV/Validate/Cliente.php
+++ library/CV/Validate/Cliente.php
@@ -1,35 +1,34 @@
 <?php
-require_once 'Zend/Validate/Abstract.php';
 
 class CV_Validate_Cliente extends Zend_Validate_Abstract
 {
     const MSG_CLIENTE = 'msgValidateCliente';
+    protected $_messageTemplates = NULL;
 
-    protected $_messageTemplates = array(
-        self::MSG_CLIENTE => "Invalid Client",
-    );
 
     public function isValid($value)
     {
+        $errorMsg = Zend_Registry::getInstance()->language->_('Cliente Inválido');
+        $this->_messageTemplates = array(self::MSG_CLIENTE => $errorMsg);
         $this->_setValue($value);
 
-		$clientes = new Clientes();
-		try
-		{
-			$codCliente = $clientes->getClienteByName($value);
-			if ($codCliente != NULL)
-				return true;
-			else
-			{
-		            $this->_error(self::MSG_CLIENTE);
-		            return false;
-			}
-		}
-		catch (Zend_Exception $e)
+        $clientes = new Clientes();
+        try
+        {
+            $codCliente = $clientes->getClienteByName($value);
+            if ($codCliente != NULL)
+                return true;
+            else
+            {
+                $this->_error(self::MSG_CLIENTE);
+                return false;
+            }
+        }
+        catch (Zend_Exception $e)
         {
             throw new Exception($e->getMessage());
             return false;
-		}
+        }
 
     }
 }
